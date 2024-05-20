088848b6a97b38f69347a02a30997c27c9e49f67
Bruno Lattanzio <brunoj.lattanzio@gmail.com>
2024-05-20 20:14:06 -0300

    config vite fixed

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    port: 8090,
+    port: process.env.PORT || 8090,
     strict: true
   },
   server: {
-   port: 8090,
+   port: process.env.PORT || 8090,
    strictPort: true,
    host: true,
    //origin: "http://0.0.0.0:8080",
-   origin: "http://localhost:8090",
+   origin: `http://localhost:${process.env.PORT || 8090}`,
   }
 })
