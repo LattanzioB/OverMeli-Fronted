85ecb38c597e47f0803f56a398833a63e587a232
Bruno Lattanzio <brunoj.lattanzio@gmail.com>
2024-05-20 21:39:35 -0300

    adding env file

--- config.jsx
+++ config.jsx
@@ -1,8 +1,9 @@
+/*
 const {
     LOGINAPI_URL
   } = process.env;
-
-  const loginApiUrl = LOGINAPI_URL || 'http://localhost:8000'
+  */
+  const loginApiUrl = process.env.LOGINAPI_URL || 'http://localhost:8000'
   console.log("Login Api URL: ", loginApiUrl)
   
   export { loginApiUrl };
