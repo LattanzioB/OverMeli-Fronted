e598f481bb177d1ccacda8546a2714cc37d3948f
Bruno Lattanzio <brunoj.lattanzio@gmail.com>
2024-06-26 19:23:40 -0300

    url edited + axiosconfig

--- config.jsx
+++ config.jsx
@@ -3,7 +3,7 @@ const {
     LOGINAPI_URL
   } = process.env;
   */
-  const loginApiUrl = process.env.REACT_APP_LOGINAPI_URL || "overmeli-loginapi-production.up.railway.app"
+  const loginApiUrl = process.env.REACT_APP_LOGINAPI_URL || "https://overmeli-loginapi-production.up.railway.app"
 
   console.log("Login Api URL: ", loginApiUrl)
   
--- context/userContext.jsx
+++ context/userContext.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import PropTypes from 'prop-types';
 import { createContext, useState, useEffect, useContext } from 'react';
+import '../src/axiosConfig'
 
 export const UserContext = createContext({});
 
