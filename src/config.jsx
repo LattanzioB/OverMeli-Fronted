9af359a046bb2195d2bdff1451b879c8e9464ec6
Bruno Lattanzio <brunoj.lattanzio@gmail.com>
2024-07-11 19:32:50 -0300

    config urls

--- src/config.jsx
+++ src/config.jsx
@@ -1,7 +1,7 @@
 
-  const loginApiUrl = process.env.REACT_APP_LOGINAPI_URL || "https://overmeli-loginapi-production.up.railway.app"
+  const loginApiUrl = process.env.REACT_APP_LOGINAPI_URL || "http://localhost:8000"//"https://overmeli-loginapi-production.up.railway.app"
 
-  const meliApiUlr = process.env.REACT_APP_MELIAPI_URL || "https://overmeli-meliapi-production.up.railway.app"
+  const meliApiUlr = process.env.REACT_APP_MELIAPI_URL || "http://localhost:8010"//"https://overmeli-meliapi-production.up.railway.app"
 
   console.log("Login Api URL: ", loginApiUrl)
   
